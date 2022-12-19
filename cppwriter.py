331fcba8edcf14738a3a9b541b2a34ca537fce51
Berkan Höke <berkanh@gmail.com>
2022-12-19 14:18:12 +0000

    cpp writer update

--- cppwriter.py
+++ cppwriter.py
@@ -4,9 +4,11 @@ import os
 
 
 def np2str(arr):
-    str_arr = np.array2string(arr)
+    str_arr = np.array2string(arr, separator= ",")
     str_arr = str_arr.replace("[", "{").replace("]","}")
-    str_arr = str_arr.replace("  ", ", ")
+    # str_arr = str_arr.replace("\n", ", ")
+    # str_arr = str_arr.replace("  ", ", ")
+    # str_arr = str_arr.replace(" ", ", ")
     return str_arr
 
 class GenericExporter(ABC):
@@ -54,19 +56,19 @@ class PolynomialRegExporter(GenericExporter):
         self.c_str += f"float COEFFS[NUM_FEATURES] = {self.coeff_str};\n"
         self.c_str += f"float OFFSET = {self.offset};\n"
 
-class DTRegressorExporter():
-    def __init__(self) -> None:
+class DTRegressorExporter(GenericExporter):
+    def __init__(self, regressor) -> None:
+        self.regressor = regressor
         super().__init__()
+        self.init_params()
     def init_params(self):
-        pass
+        self.tree = self.regressor.tree_
+        self.values = np.squeeze(self.tree.value)
     def create_string(self):
-        self.c_str += f"#define NUM_FEATURES {0}\n"
-        self.c_str += f"#define NUM_NODES {0}\n"
-        self.c_str += f"int LEFT_CHILDREN[NUM_NODES] = {self.left_children};\n"
-        self.c_str += f"int RIGHT_CHILDREN[NUM_NODES] = {self.right_children} ;\n"
-        self.c_str += f"int SPLIT_FEATURE[NUM_NODES] = {self.split_features};\n"
-        self.c_str += f"float THRESHOLDS[NUM_NODES] = {self.thresholds};\n"
-        self.c_str += f"int COEFFS[NUM_NODES] = {self.coeffs};\n"
-
-
-        
+        self.c_str += f"#define NUM_FEATURES {self.regressor.n_features_in_}\n"
+        self.c_str += f"#define NUM_NODES {self.tree.node_count}\n"
+        self.c_str += f"const int LEFT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_left)};\n"
+        self.c_str += f"const int RIGHT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_right)} ;\n"
+        self.c_str += f"const int SPLIT_FEATURE[NUM_NODES] = {np2str(self.tree.feature)};\n"
+        self.c_str += f"const float THRESHOLDS[NUM_NODES] = {np2str(self.tree.threshold)};\n"
+        self.c_str += f"const float VALUES[NUM_NODES] = {np2str(self.values)};\n"
