ce2d8d4f9c29da021aa6a41c1467f9ec8b41a180
Berkan Höke <berkanh@gmail.com>
2023-03-03 16:18:09 +0000

    Update decision in cppwriter.py

--- classification/cppwriter.py
+++ classification/cppwriter.py
@@ -33,7 +33,20 @@ class BayesExporter(GenericExporter):
     
 class DTClassifierExporter(GenericExporter):
     def __init__(self, dt_classifier) -> None:
+        self.dt_classifier = dt_classifier
         super().__init__()
+        self.init_params()
+    def init_params(self):
+        self.tree = self.dt_classifier.tree_
+        self.values = np.squeeze(self.tree.value)
+    def create_string(self):
+        self.c_str += f"#define NUM_FEATURES {self.dt_classifier.n_features_in_}\n"
+        self.c_str += f"#define NUM_NODES {self.tree.node_count}\n"
+        self.c_str += f"const int LEFT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_left)};\n"
+        self.c_str += f"const int RIGHT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_right)} ;\n"
+        self.c_str += f"const int SPLIT_FEATURE[NUM_NODES] = {np2str(self.tree.feature)};\n"
+        self.c_str += f"const float THRESHOLDS[NUM_NODES] = {np2str(self.tree.threshold)};\n"
+        self.c_str += f"const float VALUES[NUM_NODES] = {np2str(self.values)};\n"
 
 class KNNExporter(GenericExporter):
     def __init__(self, knn_classifier) -> None:
@@ -42,32 +55,3 @@ class KNNExporter(GenericExporter):
 class SVMExporter(GenericExporter):
     def __init__(self, svm_classifier) -> None:
         super().__init__()
-
-class PolynomialRegExporter(GenericExporter):
-    def __init__(self, regressor) -> None:
-        self.regressor = regressor
-        super().__init__()
-    def init_params(self):
-        self.coeff_str = np2str(self.regressor.coef_)
-        self.offset = self.regressor.intercept_ if self.regressor.intercept_ else 0
-    def create_string(self):
-        self.c_str += f"#define NUM_FEATURES {self.regressor.n_features_in_}\n"
-        self.c_str += f"float COEFFS[NUM_FEATURES] = {self.coeff_str};\n"
-        self.c_str += f"float OFFSET = {self.offset};\n"
-
-class DTRegressorExporter(GenericExporter):
-    def __init__(self, regressor) -> None:
-        self.regressor = regressor
-        super().__init__()
-        self.init_params()
-    def init_params(self):
-        self.tree = self.regressor.tree_
-        self.values = np.squeeze(self.tree.value)
-    def create_string(self):
-        self.c_str += f"#define NUM_FEATURES {self.regressor.n_features_in_}\n"
-        self.c_str += f"#define NUM_NODES {self.tree.node_count}\n"
-        self.c_str += f"const int LEFT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_left)};\n"
-        self.c_str += f"const int RIGHT_CHILDREN[NUM_NODES] = {np2str(self.tree.children_right)} ;\n"
-        self.c_str += f"const int SPLIT_FEATURE[NUM_NODES] = {np2str(self.tree.feature)};\n"
-        self.c_str += f"const float THRESHOLDS[NUM_NODES] = {np2str(self.tree.threshold)};\n"
-        self.c_str += f"const float VALUES[NUM_NODES] = {np2str(self.values)};\n"
