a574fa652f9a1f82f592006ceaef596ad2c05862
bhoke <berkanh@gmail.com>
2024-04-25 19:32:57 +0100

    kmeans implementation added

--- src/sklearn2c/clustering/kmeans.py
+++ src/sklearn2c/clustering/kmeans.py
@@ -1,9 +1,28 @@
-from sklearn.datasets import make_blobs
+import joblib
 from sklearn.cluster import KMeans
-from cppwriter import kMeansExporter
+from .clus_writer import kMeansExporter
 
-blobs, labels = make_blobs(n_samples=100, n_features=2, centers= 2, random_state=42)
-kmeans = KMeans(n_clusters = 2)
-clustered = kmeans.fit(blobs)
-kMeansWriter = kMeansExporter(clustered)
-kMeansWriter.export2file('kmeans_config.h')
+class Kmeans:
+    def __init__(self, **kwargs) -> None:
+       self.clus = KMeans(**kwargs)
+    
+    def train(self, train_samples, save_path = None):
+        self.clus.fit(train_samples, save_path)
+        if save_path:
+            joblib.dump(self, save_path)
+
+    def predict(self, test_samples):
+        result = self.clus.predict(test_samples)
+        return result
+    
+    @staticmethod
+    def load(filename:str) -> "Kmeans":
+        with open(filename, "rb") as joblib_file:
+            model = joblib.load(joblib_file)
+        if not isinstance(model, Kmeans):
+            raise TypeError(f"Expected an object of type Kmeans, but got {type(model)} instead.")
+        return model
+        
+    def export(self, filename = 'kmeans_clus_config'):
+        kMeansWriter = kMeansExporter(self.clus)
+        kMeansWriter.export(filename)
